c393ac0bcc4a99c2b3bf188d1e7e57d20841bbf0
zt l <zt l>
2016-08-19 15:59:14 +0800

    1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.labelDepth = new System.Windows.Forms.Label();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.textBoxInfo2 = new System.Windows.Forms.TextBox();
+            this.renderWindow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.renderWindow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(79, 288);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(62, 18);
+            this.labelColor.TabIndex = 1;
+            this.labelColor.Text = "label1";
+            // 
+            // labelDepth
+            // 
+            this.labelDepth.AutoSize = true;
+            this.labelDepth.Location = new System.Drawing.Point(82, 356);
+            this.labelDepth.Name = "labelDepth";
+            this.labelDepth.Size = new System.Drawing.Size(62, 18);
+            this.labelDepth.TabIndex = 2;
+            this.labelDepth.Text = "label1";
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(394, 55);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.Size = new System.Drawing.Size(241, 274);
+            this.textBoxConsole.TabIndex = 3;
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(85, 493);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(220, 238);
+            this.textBoxInfo.TabIndex = 4;
+            // 
+            // textBoxInfo2
+            // 
+            this.textBoxInfo2.Location = new System.Drawing.Point(468, 493);
+            this.textBoxInfo2.Multiline = true;
+            this.textBoxInfo2.Name = "textBoxInfo2";
+            this.textBoxInfo2.Size = new System.Drawing.Size(147, 128);
+            this.textBoxInfo2.TabIndex = 5;
+            // 
+            // renderWindow
+            // 
+            this.renderWindow.Location = new System.Drawing.Point(865, 143);
+            this.renderWindow.Name = "renderWindow";
+            this.renderWindow.Size = new System.Drawing.Size(640, 480);
+            this.renderWindow.TabIndex = 6;
+            this.renderWindow.TabStop = false;
+            this.renderWindow.Paint += new System.Windows.Forms.PaintEventHandler(this.renderWindow_Paint);
+            this.renderWindow.Resize += new System.EventHandler(this.renderWindow_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1609, 784);
+            this.Controls.Add(this.renderWindow);
+            this.Controls.Add(this.textBoxInfo2);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.labelDepth);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.renderWindow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Label labelDepth;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.TextBox textBoxInfo2;
+        private System.Windows.Forms.PictureBox renderWindow;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SampleDX;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private D2D1Render render = new D2D1Render();
+        private static readonly object _renderlock=new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            render.SetHWND(renderWindow);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PXCMSession ps = PXCMSession.CreateInstance();
+            PXCMSenseManager psm = PXCMSenseManager.CreateInstance();
+            psm.session.SetCoordinateSystem(PXCMSession.CoordinateSystem.COORDINATE_SYSTEM_REAR_OPENCV);
+            psm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 640, 480, 30);
+            psm.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_DEPTH, 320, 240, 30);
+            psm.Enable3DScan();
+            var scan = psm.Query3DScan();
+            psm.Init();
+            PXCMSession.ImplDesc desc1=new PXCMSession.ImplDesc();
+            desc1.group = PXCMSession.ImplGroup.IMPL_GROUP_SENSOR;
+            desc1.subgroup = PXCMSession.ImplSubgroup.IMPL_SUBGROUP_VIDEO_CAPTURE;
+            for(int m=0; ; m++)
+            {
+                PXCMSession.ImplDesc desc2 = new PXCMSession.ImplDesc();
+                if (ps.QueryImpl(desc1, m, out desc2).IsError()) break;
+                textBoxConsole.AppendText("Moudles"+m.ToString()+":"+desc2.friendlyName+"\n");
+                PXCMCapture capture;
+                var status = ps.CreateImpl<PXCMCapture>(desc2, out capture);
+                if (status.IsError()) continue;
+                PXCMCapture.StreamType strmtp = PXCMCapture.StreamType.STREAM_TYPE_DEPTH | 
+                                                PXCMCapture.StreamType.STREAM_TYPE_COLOR;
+                for(int d=0; ; d++)
+                {
+                    PXCMCapture.DeviceInfo info;
+                    PXCMCapture.Device device;
+                    PXCMCapture.Device.StreamProfileSet prfst;
+                    if (capture.QueryDeviceInfo(d, out info).IsError()) break;
+                    device = capture.CreateDevice(d);
+                    for(int p=0; ; ++p)
+                    {
+                        var stts = device.QueryStreamProfileSet(strmtp, p, out prfst);
+                        if (stts.IsError()) break;
+                        textBoxInfo.AppendText("STRM::COLOR"+p+":"+
+                                                prfst.color.imageInfo.width+"X"+
+                                                prfst.color.imageInfo.height+"\n");
+                        textBoxInfo.AppendText("STRM::DEPTH" + p + ":" +
+                                                prfst.depth.imageInfo.width + "X" +
+                                                prfst.depth.imageInfo.height + "\n");
+                    }
+                    textBoxConsole.AppendText("Devices" + d.ToString() + ":" + info.name+"\n");
+                    if ((info.streams & PXCMCapture.StreamType.STREAM_TYPE_COLOR) != 0)
+                        textBoxInfo.AppendText("Color Stream\n");
+                    if((info.streams & PXCMCapture.StreamType.STREAM_TYPE_DEPTH) != 0)
+                        textBoxInfo.AppendText("Depth Stream\n");
+                }
+                capture.Dispose();
+            }
+            PXCMCapture.DeviceInfo myinfo = new PXCMCapture.DeviceInfo();
+            psm.QueryCaptureManager().QueryDevice().QueryDeviceInfo(out myinfo);//??
+            textBoxInfo2.AppendText(myinfo.model.ToString());
+            while (true)
+            {
+                var a = psm.AcquireFrame(true);
+                var status = psm.AcquireFrame(true).IsError();
+                if (status) break;
+                PXCMCapture.Sample sample = psm.QuerySample();
+                if (sample != null)
+                {
+                    if (sample.color != null)
+                    {
+                        sample.color.QueryInfo();
+                        labelColor.Text = psm.captureManager.QueryFrameIndex().ToString();
+                    }
+                    if (sample.depth != null)
+                    {
+                        labelDepth.Text = psm.captureManager.QueryNumberOfFrames().ToString();
+                    }
+                    if (!scan.IsScanning()) break;
+                }
+                
+                psm.ReleaseFrame();
+            }
+            psm.Dispose();
+        }
+
+        private void renderWindow_Resize(object sender, EventArgs e)
+        {
+            lock (_renderlock)
+            {
+                render.UpdatePanel();
+            }
+        }
+
+        private void renderWindow_Paint(object sender, PaintEventArgs e)
+        {
+            lock (_renderlock)
+            {
+                render.UpdatePanel();
+            }
+        }
+    }
+}
